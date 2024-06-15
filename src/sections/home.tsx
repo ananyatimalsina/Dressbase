081ccc8ed0cdac64fcf24732201ba95e1503f22c
Lite <ananyatim333@gmail.com>
2024-06-15 14:47:16 +0200

    Major Rewrite
    - Remove Svgs for Image Border effect and replace them with divs
    
    - Add custom cursor on Image change
    
    - Implement better Image Rendering

--- src/sections/home.tsx
+++ src/sections/home.tsx
@@ -1,100 +1,133 @@
-import model_original from "../assets/model_original.png";
+import { useEffect, useRef, useState } from "react";
+
 import "./home.css";
 
+import model_original from "../assets/model_original.png";
+import model_glasses from "../assets/model_glasses.png";
+import model_top from "../assets/model_top.png";
+import model_shorts from "../assets/model_shorts.png";
+
 function Home() {
+  const [showAnimatedCursor, setShowAnimatedCursor] = useState(false);
+
+  const modelDefault = [
+    model_original,
+    model_original,
+    model_original,
+    model_original,
+  ];
+
+  const [model, setModel] = useState([
+    model_original,
+    model_original,
+    model_original,
+    model_original,
+  ]);
+
+  const rectHead = useRef<HTMLDivElement | null>(null);
+  const rectTop = useRef<HTMLDivElement | null>(null);
+  const rectShorts = useRef<HTMLDivElement | null>(null);
+
+  const cursor = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    document.addEventListener("mousemove", (event) => {
+      if (cursor.current) {
+        cursor.current.style.left = event.clientX + "px";
+        cursor.current.style.top = event.clientY + "px";
+      }
+    });
+
+    if (rectHead.current) {
+      rectHead.current.addEventListener("mouseover", (_event) => {
+        setModel((prevModel) => [model_glasses, ...prevModel.slice(1)]);
+        setShowAnimatedCursor(true);
+      });
+      rectHead.current.addEventListener("mouseout", (_event) => {
+        setModel(modelDefault);
+        setShowAnimatedCursor(false);
+      });
+    }
+
+    if (rectTop.current) {
+      rectTop.current.addEventListener("mouseover", (_event) => {
+        setModel((prevModel) => [
+          prevModel[0],
+          model_top,
+          ...prevModel.slice(2),
+        ]);
+        setShowAnimatedCursor(true);
+      });
+      rectTop.current.addEventListener("mouseout", (_event) => {
+        setModel(modelDefault);
+        setShowAnimatedCursor(false);
+      });
+    }
+
+    if (rectShorts.current) {
+      rectShorts.current.addEventListener("mouseover", (_event) => {
+        setModel((prevModel) => [
+          ...prevModel.slice(0, 2),
+          model_shorts,
+          ...prevModel.slice(3),
+        ]);
+        setShowAnimatedCursor(true);
+      });
+      rectShorts.current.addEventListener("mouseout", (_event) => {
+        setModel(modelDefault);
+        setShowAnimatedCursor(false);
+      });
+    }
+  }, []);
+
   return (
-    <div className="homeContainer">
-      <img
-        className="imgModelOriginal"
-        src={model_original}
-        alt="Model Original"
-      />
-      <svg
-        className="mask"
-        xmlns="http://www.w3.org/2000/svg"
-        width="383"
-        height="583"
-        fill="none"
-        viewBox="0 0 383 583"
-      >
-        <clipPath id="squircleClipPath">
-          <rect width="300" height="90" x="1.5" y="10.5" rx="45" />
-          <rect width="300" height="90" x="84.5" y="120.5" rx="45" />
-          <rect width="300" height="90" x="84.5" y="230.5" rx="45" />
-          <rect width="300" height="140" x="84.5" y="340.5" rx="48.5" />
-          <rect width="300" height="90" x="1.5" y="500.5" rx="45" />
-        </clipPath>
-        <rect
-          width="300"
-          height="90"
-          x="1.5"
-          y="1.5"
-          stroke="#000"
-          strokeWidth="3"
-          rx="45"
-        />
-        <rect
-          width="300"
-          height="90"
-          x="84.5"
-          y="111.5"
-          stroke="#000"
-          strokeWidth="3"
-          rx="45"
-        />
-        <rect
-          width="300"
-          height="90"
-          x="84.5"
-          y="221.5"
-          stroke="#000"
-          strokeWidth="3"
-          rx="45"
-        />
-        <rect
-          width="300"
-          height="140"
-          x="84.5"
-          y="331.5"
-          stroke="#000"
-          strokeWidth="3"
-          rx="48.5"
-        />
-        <rect
-          width="300"
-          height="90"
-          x="1.5"
-          y="491.5"
-          stroke="#000"
-          strokeWidth="3"
-          rx="45"
-        />
-      </svg>
-      <div className="branding">
-        <div
-          className="brandingText"
-          style={{
-            top: "0px",
-            left: "0",
-            position: "absolute",
-            fontSize: "8rem",
-          }}
-        >
-          DRESS
+    <>
+      {showAnimatedCursor && <div className="cursor" ref={cursor}></div>}
+      <div className="homeContainer">
+        <div className="imgContainer">
+          <div className="imgWrapper">
+            <img src={model[0]} alt="ModelHead" />
+            <div className="overlay" ref={rectHead}></div>
+          </div>
+          <div className="imgWrapper">
+            <img src={model[1]} alt="ModelTop" />
+            <div className="overlay" ref={rectTop}></div>
+          </div>
+          <div className="imgWrapper">
+            <img src={model[2]} alt="ModelShorts" />
+            <div className="overlay" ref={rectShorts}></div>
+          </div>
+          <div className="imgWrapper">
+            <img src={model[3]} alt="ModelShoes" />
+            <div className="overlay"></div>
+          </div>
         </div>
-        <div
-          className="brandingText"
-          style={{
-            top: "340px",
-            right: "0",
-            position: "absolute",
-            fontSize: "8rem",
-          }}
-        >
-          BASE
+        <div className="branding">
+          <div
+            className="brandingText"
+            style={{
+              top: "200px",
+              left: "400px",
+              position: "absolute",
+              fontSize: "8rem",
+            }}
+          >
+            DRESS
+          </div>
+          <div
+            className="brandingText"
+            style={{
+              top: "480px",
+              right: "400px",
+              position: "absolute",
+              fontSize: "8rem",
+            }}
+          >
+            BASE
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
