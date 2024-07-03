bdcd5a623816fb6bd5583efe27f99e467522fc69
Lite <ananyatim333@gmail.com>
2024-07-03 20:46:30 +0200

    Add Image and more About description

--- src/sections/About/about.tsx
+++ src/sections/About/about.tsx
@@ -1,6 +1,7 @@
 import AboutCard from "../../components/AboutCard/AboutCard";
 
 import image from "../../assets/background.jpg";
+import niklas from "../../assets/niklas.jpg";
 
 import "./about.css";
 
@@ -24,7 +25,7 @@ function about() {
           text="Help me!"
         />
         <AboutCard
-          img={image}
+          img={niklas}
           alt="Niklas"
           title="CMO, Niklas Wenz"
           text="Mit mehrjähriger Erfahrung im Marketing und einer Leidenschaft für Innovation und Technologie gestalte ich die Marketingstrategie, die Dressbase zum Erfolg führt."
@@ -39,7 +40,7 @@ function about() {
           img={image}
           alt="Paul"
           title="CFO, Paul Schaut"
-          text="Help me!"
+          text="Mit großer Leidenschaft für Geld und Zahlen leite ich den finanziellen Sektor von Dressbase und kümmere mich um Ausgaben und Einnahmen, damit unser Start-up auf finanziell starken Beinen steht."
         />
       </div>
     </div>
